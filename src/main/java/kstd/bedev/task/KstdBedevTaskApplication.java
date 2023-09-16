e35e0e2217048e774b5184521d804497fdc74150
mskei <kei@test.com>
2023-09-16 12:18:11 +0900

    init

---
+++ src/main/java/kstd/bedev/task/KstdBedevTaskApplication.java
@@ -0,0 +1,13 @@
+package kstd.bedev.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstdBedevTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KstdBedevTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kstd/bedev/task/KstdBedevTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kstd.bedev.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KstdBedevTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
